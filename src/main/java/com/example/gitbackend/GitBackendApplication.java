a2d2e9667449796ace3ae900948b652320cc16e8
nesi <nesibelikulazhanova@gmail.com>
2025-02-06 08:58:29 +0500

    First commit

---
+++ src/main/java/com/example/gitbackend/GitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitbackend/GitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
